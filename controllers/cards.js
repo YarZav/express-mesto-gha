df35bec4415ce52c0e718ff084985fed461c4f7a
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-09-02 18:10:24 +0200

    Добавил обработку ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,7 +4,7 @@ module.exports.getCards = (req, res) => {
   Card.find({})
     .populate('owner')
     .then((cards) => res.send({ data: cards }))
-    .catch((error) => res.status(500).send({ message: `Не удалось найти карточки ${error}` }));
+    .catch(() => res.status(500).send());
 };
 
 module.exports.createCard = (req, res) => {
@@ -13,27 +13,51 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner })
     .then((card) => res.send({ data: card }))
-    .catch((error) => res.status(500).send({ message: `Не удалось создать карточку ${error}` }));
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
+      } else {
+        res.status(500).send();
+      }
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.body.id)
-    .then((card) => res.send({ data: card }))
-    .catch((error) => res.status(500).send({ message: `Не удалось удалить карточку ${error}` }));
+    .then((card) => {
+      if (card === null) {
+        res.status(404).send({ message: `Карточка с указанным ${req.params.id} не найдена.` });
+      } else {
+        res.send({ data: card });
+      }
+    })
+    .catch(() => res.status(500).send());
 };
 
 module.exports.putCardLike = (req, res) => {
   // eslint-disable-next-line no-underscore-dangle
   const owner = req.user._id;
   Card.findByIdAndUpdate(req.body.id, { $addToSet: { likes: owner } })
-    .then((card) => res.send({ data: card }))
-    .catch((error) => res.status(500).send({ message: `Не удалось лайкнуть карточку ${error}` }));
+    .then((card) => {
+      if (card === null) {
+        res.status(404).send({ message: `Передан несуществующий ${req.body.id} карточки` });
+      } else {
+        res.send({ data: card });
+      }
+    })
+    .catch(() => res.status(500).send());
 };
 
 module.exports.deleteCardLike = (req, res) => {
   // eslint-disable-next-line no-underscore-dangle
   const owner = req.user._id;
   Card.findByIdAndUpdate(req.body.id, { $pull: { likes: owner } })
-    .then((card) => res.send({ data: card }))
-    .catch((error) => res.status(500).send({ message: `Не удалось лайкнуть карточку ${error}` }));
+    .then((card) => {
+      if (card === null) {
+        res.status(404).send({ message: `Передан несуществующий ${req.body.id} карточки` });
+      } else {
+        res.send({ data: card });
+      }
+    })
+    .catch(() => res.status(500).send());
 };
--- controllers/users.js
+++ controllers/users.js
@@ -3,36 +3,72 @@ const User = require('../models/user');
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((error) => res.status(500).send({ message: `Не удалось найти пользователей ${error}` }));
+    .catch(() => res.status(500).send());
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
-    .catch((error) => res.status(500).send({ message: `Не удалось найти пользователя ${error}` }));
+    .then((user) => {
+      if (user === null) {
+        res.status(404).send({ message: `Пользователь по указанному ${req.params.id} не найден.` });
+      } else {
+        res.send({ data: user });
+      }
+    })
+    .catch(() => res.status(500).send());
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((error) => res.status(500).send({ message: `Не удалось создать пользователя ${error}` }));
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+      } else {
+        res.status(500).send();
+      }
+    });
 };
 
 module.exports.patchUsersMe = (req, res) => {
   // eslint-disable-next-line no-underscore-dangle
   const id = req.user._id;
   const { name, about } = req.body;
-  User.findByIdAndUpdate(id, { name, about })
-    .then((user) => res.send({ data: user }))
-    .catch((error) => res.status(500).send({ message: `Не удалось обновить пользователя ${error}` }));
+  User.findByIdAndUpdate(id, { name, about }, { runValidators: true })
+    .then((user) => {
+      if (user === null) {
+        res.status(404).send({ message: `Пользователь по указанному ${req.params.id} не найден.` });
+      } else {
+        res.send({ data: user });
+      }
+    })
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+      } else {
+        res.status(500).send();
+      }
+    });
 };
 
 module.exports.patchUsersMeAvatar = (req, res) => {
   // eslint-disable-next-line no-underscore-dangle
   const id = req.user._id;
   const { avatar } = req.body;
-  User.findByIdAndUpdate(id, { avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((error) => res.status(500).send({ message: `Не удалось обновить пользователя ${error}` }));
+  User.findByIdAndUpdate(id, { avatar }, { runValidators: true })
+    .then((user) => {
+      if (user === null) {
+        res.status(404).send({ message: `Пользователь по указанному ${req.params.id} не найден.` });
+      } else {
+        res.send({ data: user });
+      }
+    })
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+      } else {
+        res.status(500).send();
+      }
+    });
 };
