4adb4c388cafea81560d1eff8dc9329ab799b32a
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-09-02 14:54:39 +0200

    Создание schema, router, controller для user

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes/users');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use('/', router);
+app.listen(PORT);
