de48138d9b35f2a0a55e4e5cb98b185241cd817c
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-09-02 15:35:24 +0200

    Добавил работу с пользователем

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const router = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', router);
+
+// Temporary hack ->
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64f338ba61cfe28fa71b2ced',
+  };
+
+  next();
+});
+//
+
 app.listen(PORT);
