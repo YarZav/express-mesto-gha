bdd724f0371cc49445cebda0a1e0c3b3100ea8f6
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-09-16 18:53:37 +0200

    Добавил запросы регистрации и авторизации

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,14 @@
 const routes = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const { login } = require('../controllers/login');
+const { createUser } = require('../controllers/users');
 
 const ERROR_WRONG_DATA_CODE = 404;
 const ERROR_WRONG_DATA_MESSAGE = 'Данные не найдены.';
 
+routes.post('/signin', login);
+routes.post('/signup', createUser);
 routes.use('/users', usersRouter);
 routes.use('/cards', cardsRouter);
 routes.use('*', (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,12 @@ const usersRouter = require('express').Router();
 const {
   getUsers,
   getUser,
-  createUser,
   patchUsersMe,
   patchUsersMeAvatar,
 } = require('../controllers/users');
 
 usersRouter.get('', getUsers);
 usersRouter.get('/:id', getUser);
-usersRouter.post('', createUser);
 usersRouter.patch('/me', patchUsersMe);
 usersRouter.patch('/me/avatar', patchUsersMeAvatar);
 
