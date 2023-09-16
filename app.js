214553df25859de33b7258b07d0dbb026ceadcf5
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-09-16 19:14:18 +0200

    Удалил хардкод и добавил проверку авторизации

--- app.js
+++ app.js
@@ -9,16 +9,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// Temporary hack ->
-app.use((req, res, next) => {
-  req.user = {
-    _id: '64f338ba61cfe28fa71b2ced',
-  };
-  next();
-});
-// <- Remove it
-
 app.use('/', routes);
 
 app.listen(PORT);
--- routes/routes.js
+++ routes/routes.js
@@ -3,14 +3,17 @@ const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 const { login } = require('../controllers/login');
 const { createUser } = require('../controllers/users');
+const auth = require('../middlewares/auth');
 
 const ERROR_WRONG_DATA_CODE = 404;
 const ERROR_WRONG_DATA_MESSAGE = 'Данные не найдены.';
 
 routes.post('/signin', login);
 routes.post('/signup', createUser);
-routes.use('/users', usersRouter);
-routes.use('/cards', cardsRouter);
+
+routes.use('/users', auth, usersRouter);
+routes.use('/cards', auth, cardsRouter);
+
 routes.use('*', (req, res) => {
   res.status(ERROR_WRONG_DATA_CODE).send({ message: ERROR_WRONG_DATA_MESSAGE });
 });
