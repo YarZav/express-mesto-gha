3836db4659bbc062340903d2adab26603b75454f
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-09-17 22:01:00 +0200

    Исправил обработку ошибок

--- controllers/login.js
+++ controllers/login.js
@@ -1,8 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const { ERROR_AUTH_CODE } = require('../constants/constants');
 
-module.exports.login = (req, res) => {
+module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
   return User.findUserByCredentials(email, password)
@@ -10,7 +9,5 @@ module.exports.login = (req, res) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       res.send({ token });
     })
-    .catch((error) => {
-      res.status(ERROR_AUTH_CODE).send({ message: error.message });
-    });
+    .catch(next);
 };
