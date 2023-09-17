3e3ef6fd9df5de3011c249e562d994bf8ecb3f92
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-09-17 21:58:47 +0200

    Исправил работу с ошибками

--- errors/errorHandler.js
+++ errors/errorHandler.js
@@ -1,5 +1,14 @@
 const errorHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode || 404;
+  let statusCode;
+  if (err.name === 'MongoServerError') {
+    statusCode = 409;
+  } else {
+    statusCode = err.statusCode || 404;
+  }
+
+  console.log(err.name);
+  console.log(err.message);
+  console.log(err.statusCode);
   const { message } = err;
   res.status(statusCode).send({ message });
   next();
