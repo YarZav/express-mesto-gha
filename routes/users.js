e88ee722ae2731687c5c87949309250571f3ab46
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-09-17 13:41:38 +0200

    Исправил валидатор роута пользователей

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-const { celebrate, Joi } = require('celebrate');
 const usersRouter = require('express').Router();
 const {
   getUsers,
@@ -7,37 +6,16 @@ const {
   patchUsersMe,
   patchUsersMeAvatar,
 } = require('../controllers/users');
-const urlRegExp = require('../constants/constants');
+const {
+  usersIdRouteValidation,
+  usersMeRouteValidation,
+  usersMeAvatarRoutevalidation,
+} = require('../validator/users/users');
 
 usersRouter.get('', getUsers);
 usersRouter.get('/me', getUsersMe);
-usersRouter.get(
-  '/:id',
-  celebrate({
-    params: Joi.object().keys({
-      id: Joi.string().length(24).hex().required(),
-    }),
-  }),
-  getUser,
-);
-usersRouter.patch(
-  '/me',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-    }),
-  }),
-  patchUsersMe,
-);
-usersRouter.patch(
-  '/me/avatar',
-  celebrate({
-    body: Joi.object().keys({
-      avatar: Joi.string().pattern(urlRegExp),
-    }),
-  }),
-  patchUsersMeAvatar,
-);
+usersRouter.get('/:id', usersIdRouteValidation, getUser);
+usersRouter.patch('/me', usersMeRouteValidation, patchUsersMe);
+usersRouter.patch('/me/avatar', usersMeAvatarRoutevalidation, patchUsersMeAvatar);
 
 module.exports = usersRouter;
