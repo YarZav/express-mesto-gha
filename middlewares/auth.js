d6a9bd18d75617dc83a08623a5717da513a318bc
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-09-17 15:24:10 +0200

    Удалил дублирование констант

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,12 @@
 const jwt = require('jsonwebtoken');
-
-const ERROR_AUTH_CODE = 401;
-const ERROR_AUTH_MESSAGE = 'Необходима авторизация';
+const { ERROR_AUTH_CODE, ERROR_AUTH_MESSAGE } = require('../constants/constants');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(ERROR_AUTH_CODE)
-      .send({ message: ERROR_AUTH_MESSAGE });
+    return res.status(ERROR_AUTH_CODE).send({ message: ERROR_AUTH_MESSAGE });
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -19,9 +15,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return res
-      .status(ERROR_AUTH_CODE)
-      .send({ message: ERROR_AUTH_MESSAGE });
+    return res.status(ERROR_AUTH_CODE).send({ message: ERROR_AUTH_MESSAGE });
   }
 
   req.user = payload;
