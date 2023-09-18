0f10ce0b498d5b2592229cb5d7e89c12b0870662
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-09-18 19:36:24 +0200

    Исправил замечание по код ревью

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const routes = require('./routes/routes');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
